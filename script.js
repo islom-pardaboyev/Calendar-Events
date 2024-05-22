51635823f700aa88eb0a79c92ce9ef0fc233a75a
Islom Pardaboyev <154039167+islom-pardaboyev@users.noreply.github.com>
2024-05-22 21:27:11 +0500

    init

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// Function to call generate calendar on load
+
 window.onload = function () {
     generateCalendar();
 };
-
-// Function to generate the calendar
 function generateCalendar() {
     const calendar = document.getElementById('calendar');
 
@@ -30,5 +28,61 @@ function generateCalendar() {
         calendar.appendChild(daySquare);
     }
 }
+function showAddTaskModal() {
+    document.getElementById('addTaskModal').style.display = 'block';
+}
+function closeAddTaskModal() {
+    document.getElementById('addTaskModal').style.display = 'none';
+}
+function deleteTask(taskElement) {
+    if (confirm("Are you sure you want to delete this task?")) {
 
-function
+        taskElement.parentNode.removeChild(taskElement);
+    }
+}
+function editTask(taskElement) {
+    const newTaskDesc = prompt("Edit your task:", taskElement.textContent);
+    if (newTaskDesc !== null & newTaskDesc.trim() !== "") {
+
+        taskElement.textContent = newTaskDesc;
+    }
+}
+function addTask() {
+    const taskDate = new Date(document.getElementById('task-date').value);
+    const taskDesc = document.getElementById('task-desc').value.trim();
+
+    if (taskDesc && !isNaN(taskDate.getDate())) {
+
+        const calendarDays = document.getElementById('calendar').children;
+
+        for (let i = 0; i < calendarDays.length; i++) {
+            const day = calendarDays[i];
+    
+            if (parseInt(day.textContent) === taskDate.getDate()) {
+        
+                const taskElement = document.createElement("div");
+                taskElement.className = "task";
+                taskElement.textContent = taskDesc;
+
+        
+                taskElement.addEventListener("contextmenu", function (event) {
+                    event.preventDefault()
+                    deleteTask(taskElement)
+                });
+
+        
+                taskElement.addEventListener('click', function () {
+                    editTask(taskElement)
+                });
+
+        
+                day.appendChild(taskElement);
+                break;
+            }
+        }
+        closeAddTaskModal()
+    } else {
+
+        alert("Please enter a valid date and task description!");
+    }
+}
